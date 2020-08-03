8c707a785421189d66d94bd65ec1eeedf0cacdbe
Hiba Machfej <hiba.mashfij@gmail.com>
2020-08-03 18:57:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+const form = document.getElementById('create-task-form');
+const tasks = document.getElementById('tasks');
+
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  const newTask = document.getElementById('new-task-description').value;
+  const taskInput = document.createElement('li')
+  taskInput.innerText = `${newTask}`
+
+  tasks.appendChild(taskInput)
+  form.reset()
+})
